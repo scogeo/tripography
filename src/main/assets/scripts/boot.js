f7ed7a972cc80b8c4e48847a860671c2e31857ca
George Scott <gscott@rumbleware.com>
2013-05-20 22:41:58 -0700

    all in a days work

--- src/main/assets/scripts/boot.js
+++
@@ -1,3 +0,0 @@
-// @requirejs_config_bundles jquery, forms
-// @require require
-// @require_bundle jquery
---
+++ src/main/assets/scripts/charts/daily-histogram.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: gscott
+ * Date: 5/19/13
+ * Time: 7:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
