bd3f865108bc9d9295b710810f7c90108283b4c2
George Scott <gscott@rumbleware.com>
2013-06-09 14:57:59 -0700

    Legal Pages

---
+++ src/main/java/com/tripography/web/controller/StaticContentController.java
@@ -0,0 +1,33 @@
+package com.tripography.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * This Controller handles all of the static content on the site, it should really be implemented and driven
+ * from a configuration file and directly load markdown templates, but works for now.
+ *
+ * @author gscott
+ */
+@Controller
+public class StaticContentController {
+
+    @RequestMapping(value = {"/legal", "/legal/"}, method = RequestMethod.GET)
+    public String getLegal() {
+        return "redirect:/legal/terms";
+    }
+
+    @RequestMapping(value = {"/legal/terms"}, method = RequestMethod.GET)
+    public String getLegalTerms() {
+        return "legal/terms";
+    }
+
+    @RequestMapping(value = "/legal/privacy", method = RequestMethod.GET)
+    public String getLegalPrivacy() {
+        return "legal/privacy";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/legal/privacy.md
@@ -0,0 +1,28 @@
+# Tripography Privacy Policy
+
+Tripography
+
+This Privacy Policy was last updated June 8, 2013.
+
+## What information does Tripography collect?
+
+## Information from your vehicle's telematics system.
+
+Tripography
+
+## Information collected when you visit our website.
+
+When you
+
+## Information security
+
+
+## Is there a way to delete my information?
+
+We store your information in our cloud servers and backup systems.  You may request that your account be deleted by
+emailing <support@tripography.com> from the email address associated with your account and requesting that your
+account be deleted.
+
+## Changes
+
+This Privacy Policy may be updated from time to time
